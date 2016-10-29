0ee0b7711a2b2c90dd4b04f291ac5f31ef701ac9
Vittorio <pryderi.mail@gmail.com>
2016-10-29 11:41:38 +0100

    Improved default load/save location

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -4,11 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
@@ -21,6 +20,9 @@ import sokochan.SokochanGrid;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
 import java.util.Optional;
 
 public class Main extends Application {
@@ -67,14 +69,23 @@ public class Main extends Application {
         primaryStage.sizeToScene();
     }
 
+    private File getDefaultLocation() {
+        File folder = new File(System.getProperty("user.dir") + "\\src\\maps\\");
+
+        if (!Files.exists(folder.toPath())) {
+            folder = new File(System.getProperty("user.dir"));
+        }
+
+        return folder;
+    }
+
     public void loadGameFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Save File");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Sokoban File", "*.skb"));
 
-
         try {
-            fileChooser.setInitialDirectory(new File("C:\\Users\\Vittorio\\Projects\\JavaProjects\\Sokochan\\src\\fx"));
+            fileChooser.setInitialDirectory(getDefaultLocation());
         } finally {
             File file = fileChooser.showOpenDialog(primaryStage);
 
@@ -96,9 +107,8 @@ public class Main extends Application {
         fileChooser.setTitle("Save Game File");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Sokoban File", "*.skb"));
 
-
         try {
-            fileChooser.setInitialDirectory(new File("C:\\Users\\Vittorio\\Projects\\JavaProjects\\Sokochan\\src\\fx"));
+            fileChooser.setInitialDirectory(getDefaultLocation());
         } finally {
             File file = fileChooser.showSaveDialog(primaryStage);
 
@@ -296,5 +306,42 @@ public class Main extends Application {
         } while (error);
 
         draw();
+    }
+
+    @SuppressWarnings("unused")
+    private void showExceptionDialog(Exception ex) {
+        // credit: http://code.makery.ch/blog/javafx-dialogs-official/
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Exception Dialog");
+        alert.setHeaderText("An unhandled exception occurred!");
+        alert.setContentText("Message: " + ex.getMessage());
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(false);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        // Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        alert.showAndWait();
     }
 }
